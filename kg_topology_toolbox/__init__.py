bc1b0d67ecdf3581a5d54fc229d622581b86adb6
Alberto Cattaneo <albertoc@graphcore.ai>
2024-03-12 14:01:31 +0000

    repo setup

---
+++ kg_topology_toolbox/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2023 Graphcore Ltd. All rights reserved.
+
+"""
+A Python toolbox for Knowledge Graph topology metrics.
+"""
+
+from .topology_toolbox import *  # NOQA:F401,E402,F403
